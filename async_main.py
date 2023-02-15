c8b5b3aad020c4d2c2ef4c637b63fe2e9d08bb8f
efabritz <katya-belova@inbox.ru>
2023-02-15 15:17:49 +0100

    version2

--- async_main.py
+++ async_main.py
@@ -1,4 +1,6 @@
 import asyncio
+from pprint import pprint
+
 import aiohttp
 import datetime
 from models import engine, Session, Base, SwapiPeople
@@ -7,8 +9,6 @@ async def get_people(session, people_id):
     print(f'{people_id=} started')
     async with session.get(f'https://swapi.dev/api/people/{people_id}') as response:
         json_data = await response.json()
-        if "detail" in json_data:
-            return {}
         print(f'{people_id=} finished')
         return json_data
 
@@ -21,8 +21,9 @@ async def follow_url(session, url):
 
 async def paste_to_db(results):
     swapi_people = [SwapiPeople(name=item['name'], birth_year=item['birth_year'], created=datetime.datetime.fromisoformat(item['created'][:-1]), edited=datetime.datetime.fromisoformat(item['edited'][:-1]), eye_color=item['eye_color'],films=item['films'], gender=item['gender'],
-                                hair_color=item['hair_color'], height=int(item['height']), homeworld=item['homeworld'], mass=int(item['mass']), skin_color=item['skin_color'], species=item['species'], starships=item['starships'],
-                                vehicles=item['vehicles'], url=item['url']) for item in results if item is not None]
+                                hair_color=item['hair_color'], height=item['height'], homeworld=item['homeworld'], mass=item['mass'], skin_color=item['skin_color'], species=item['species'], starships=item['starships'],
+                                vehicles=item['vehicles'], url=item['url']) for item in results if 'detail' not in item]
+
     async with Session() as session:
         session.add_all(swapi_people)
         await session.commit()
@@ -30,52 +31,48 @@ async def paste_to_db(results):
 async def add_species(session, results):
     new_results = []
     for result in results:
-        if not result:
-            return
-        species = result['species']
-        coros_species = [follow_url(session, specie) for specie in species]
-        res_species = await asyncio.gather(*coros_species)
-        final_species = [item['name'] for item in res_species]
-        result['species'] = list_to_str(final_species)
+        if 'detail' not in result:
+            species = result['species']
+            coros_species = [follow_url(session, specie) for specie in species]
+            res_species = await asyncio.gather(*coros_species)
+            final_species = [item['name'] for item in res_species]
+            result['species'] = list_to_str(final_species)
         new_results.append(result)
     return new_results
 
 async def add_vehicles(session, results):
     new_results = []
     for result in results:
-        if not result:
-            return
-        vehicles = result['vehicles']
-        coros_vehicles = [follow_url(session, vehilce) for vehilce in vehicles]
-        res_vehicles = await asyncio.gather(*coros_vehicles)
-        final_vehicles = [item['name'] for item in res_vehicles]
-        result['vehicles'] = list_to_str(final_vehicles)
+        if 'detail' not in result:
+            vehicles = result['vehicles']
+            coros_vehicles = [follow_url(session, vehilce) for vehilce in vehicles]
+            res_vehicles = await asyncio.gather(*coros_vehicles)
+            final_vehicles = [item['name'] for item in res_vehicles]
+            result['vehicles'] = list_to_str(final_vehicles)
         new_results.append(result)
     return new_results
 
 async def add_films(session, results):
     new_results = []
     for result in results:
-        if not result:
-            return
-        films = result['films']
-        coros_films = [follow_url(session, film) for film in films]
-        res_films = await asyncio.gather(*coros_films)
-        final_films = [item['title'] for item in res_films]
-        result['films'] = list_to_str(final_films)
+        if 'detail' not in result:
+            films = result['films']
+            coros_films = [follow_url(session, film) for film in films]
+            res_films = await asyncio.gather(*coros_films)
+            final_films = [item['title'] for item in res_films]
+            result['films'] = list_to_str(final_films)
         new_results.append(result)
     return new_results
 
 async def add_starships(session, results):
     new_results = []
     for result in results:
-        if not result:
-            return
-        starships = result['starships']
-        coros_starships = [follow_url(session, starship) for starship in starships]
-        res_starships = await asyncio.gather(*coros_starships)
-        final_starships = [item['name'] for item in res_starships]
-        result['starships'] = list_to_str(final_starships)
+        if 'detail' not in result:
+            starships = result['starships']
+            coros_starships = [follow_url(session, starship) for starship in starships]
+            res_starships = await asyncio.gather(*coros_starships)
+            final_starships = [item['name'] for item in res_starships]
+            result['starships'] = list_to_str(final_starships)
         new_results.append(result)
     return new_results
 
@@ -91,18 +88,17 @@ async def main():
     session = aiohttp.ClientSession()
 
     coros = []
-    for item in range(1, 11):
+    for item in range(1, 83):
         coros.append(get_people(session, item))
     results = await asyncio.gather(*coros)
-
-    for item in results:
-        print(item)
 
     coros_sub = []
     coros_sub.append(add_species(session, results))
     coros_sub.append(add_vehicles(session, results))
     coros_sub.append(add_films(session, results))
     coros_sub.append(add_starships(session, results))
+
+
     resulst_with_sublinks = await asyncio.gather(*coros_sub)
 
     asyncio.create_task(paste_to_db(resulst_with_sublinks[0]))
--- models.py
+++ models.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-PG_DSN = 'postgresql+asyncpg://ekaterina:1234@127.0.0.1:5430/async'
+PG_DSN = 'postgresql+asyncpg://ekaterina:1234@127.0.0.1:5432/async'
 
 engine = create_async_engine(PG_DSN)
 Session = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
@@ -24,9 +24,9 @@ class SwapiPeople(Base):
     films = Column(String)
     gender = Column(String)
     hair_color = Column(String)
-    height = Column(Integer)
+    height = Column(String)
     homeworld = Column(String)
-    mass = Column(Integer)
+    mass = Column(String)
     skin_color = Column(String)
     species = Column(String)
     starships = Column(String)
